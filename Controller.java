ad324e50e63dc1d5f76c1c20fd7ffe9007afe9fe
karbudzik <karolinabudzik92@gmail.com>
2020-02-28 12:33:41 +0100

    Change exception handling in Controller

--- Controller.java
+++ Controller.java
@@ -12,50 +12,43 @@ public class Controller {
         myValidator = new Validator();
 
         boolean isRunning = true;
-        
-        // czy mogę wyjątki uwzględnione w todoMatrix wyłapać tutaj? i jak?
         myMatrix.addItemsFromFile("todo_items_read_test.csv");
-        
         myView.printMatrix(myMatrix);
         myView.printMainMenu();
         
         while (isRunning) {
             String input = myView.getInput("Your choice: ");
-            
-            // czy podzielić te bloki kodu na mniejsze w jakiś sposób?
-            if (input.equals("0")) {
-                isRunning = false;
-                myMatrix.saveItemsToFile("todo_items_write_test.csv");
-                myView.printMessage("See you next time!");
-            } 
-            else if (input.equals("1")) {
-                try {
+            try {
+                if (input.equals("0")) {
+                    isRunning = false;
+                    myMatrix.saveItemsToFile("todo_items_write_test.csv");
+                    myView.printMessage("See you next time!");
+                } 
+                else if (input.equals("1")) {
                     TodoItem temporaryItem = myValidator.getNewItemDetails();
                     myMatrix.addItem(temporaryItem.getTitle(), temporaryItem.getDeadline(), temporaryItem.getIsImportant());
                     myView.printMatrix(myMatrix);
                     myView.printMainMenu();
-                } catch (DateTimeException e) {
-                    myView.printMessage("There is no such date!");
                 } 
-            } 
-            else if (input.equals("2")) {
-                try {
+                else if (input.equals("2")) {
                     String quarterName = myValidator.getQuarterName(); 
                     int index = myValidator.getIndex();
                     myMatrix.markAsDone(quarterName, index);
                     myView.printMatrix(myMatrix);
                     myView.printMainMenu();
-                } catch (IndexOutOfBoundsException e) {
-                    myView.printMessage("There is no item with given index in this quarter.");
                 } 
+                else if (input.equals("3")) {
+                    myMatrix.archiveItems();
+                    myView.printMessage("Done! If you had any finished items in this quarter, they have been archived!");
+                }  
+                else {
+                    myView.printMessage("Wrong input, try again!");
+                }
+            } catch (DateTimeException e) {
+                myView.printMessage("There is no such date!");
+            } catch (IndexOutOfBoundsException e) {
+                myView.printMessage("There is no item with given index in this quarter.");
             } 
-            else if (input.equals("3")) {
-                myMatrix.archiveItems();
-                // jaki wyjątek mogę podnieść tutaj, żeby zakomunikować, że nie mamy co archiwizować?
-            }  
-            else {
-                myView.printMessage("Wrong input!");
-            }
         }
     }
 }
